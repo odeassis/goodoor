e1b0affb95c3c9953fdd63448da3e1c1edbc15f7
odeassis <franciscodeassisc.santos@gmail.com>
2024-07-10 16:34:21 -0300

    feat: Update main function to initialize configuration and logger

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "github.com/odeassis/goodoor/routes"
+import (
+	"github.com/odeassis/goodoor/config"
+	"github.com/odeassis/goodoor/routes"
+)
+
+var (
+	logger *config.Looger
+)
 
 func main() {
+
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config initialize error: %v", err)
+		return
+	}
+
 	routes.Initialize()
 }
