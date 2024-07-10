da90351052913a624d9c6402274b76d34007a644
odeassis <franciscodeassisc.santos@gmail.com>
2024-07-10 18:03:21 -0300

    feat: Initialize handler package with configuration and logger setup

--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,16 @@
 package handler
+
+import (
+	"github.com/odeassis/goodoor/config"
+	"gorm.io/gorm"
+)
+
+var (
+	db     *gorm.DB
+	logger *config.Looger
+)
+
+func Init() {
+	logger = config.GetLogger("handler")
+	db = config.GetPostgreSQL()
+}
