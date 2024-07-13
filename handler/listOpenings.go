ea96e0de8e26ff411a36be4be22dc6706dd748a1
odeassis <franciscodeassisc.santos@gmail.com>
2024-07-13 08:40:17 -0300

    feat: Retrieve and list openings in ListOpenings handler

--- handler/listOpenings.go
+++ handler/listOpenings.go
@@ -4,10 +4,16 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/odeassis/goodoor/schemas"
 )
 
 func ListOpenings(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"Message": "GET openings",
-	})
+	opening := []schemas.Opening{}
+
+	if err := db.Find(&opening).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "an error in list opening")
+		return
+	}
+
+	sendSuccess(ctx, "list-openings", opening)
 }
