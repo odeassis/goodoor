1a41b9b31958da2c58fc6ad3a85bf01bb2e4f88a
odeassis <franciscodeassisc.santos@gmail.com>
2024-07-13 08:40:29 -0300

    feat: Add error handling and data retrieval for ShowOpening handler

--- handler/showOpening.go
+++ handler/showOpening.go
@@ -1,13 +1,27 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/odeassis/goodoor/schemas"
 )
 
 func ShowOpening(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET opening",
-	})
+	id := ctx.Query("id")
+
+	if id == "" {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("opening with id: %s is not found", id))
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, fmt.Sprintf("error show opening with id: %s", id))
+		return
+	}
+
+	sendSuccess(ctx, "show-opening", opening)
 }
